c37dcac58c9909b52fbf7fbbb6bfbaf578697212
Nathan Joseph <nathanjj625@gmail.com>
2024-04-06 14:45:58 -0500

    Basic Flask Implementation and First Excel Parsing

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 class Hero:
-    def __init__(self, class1, class2, stored_deck):
+    def __init__(self, name, class1, class2, stored_deck):
+        self.name = name
         self.class1 = class1
         self.class2 = class2
         self.stored_deck = stored_deck
@@ -13,7 +14,8 @@ class HeroClass:
 
 # create a database where all the cards for each class is mapped. Find some way to save what 
 
-hero_db = {}
+hero_db = [] # this is the list for every pvz zombies hero
+hero_class_db = [] # this is the list for every pvz plant (zombies will be worked on later)
 
 smarty = HeroClass("Smarty", [])
 megagrow = HeroClass("Megagrow", [])
@@ -21,17 +23,41 @@ kabloom = HeroClass("Kabloom", [])
 solar = HeroClass("Solar", [])
 guardian = HeroClass("Guardian", [])
 
+hero_class_db.extend([smarty, megagrow, kabloom, solar, guardian])
 
-green_shadow = Hero(smarty, megagrow, [])
+
+green_shadow = Hero("Green Shadow", smarty, megagrow, [])
+spudow = Hero("Spudow", guardian, kabloom, [])
+solar_flare = Hero("Solar Flare", solar, kabloom, [])
+rose = Hero("Rose", solar, smarty, [])
+beta_citron = Hero("Beta Carrotina / Citron", guardian, smarty, [])
+chompzilla = Hero("Chompzilla", solar, megagrow, [])
+captain_combustible = Hero("Captain Combustible", megagrow, kabloom, [])
+grass_knuckles = Hero("Grass Knuckles", megagrow, guardian, [])
+wallknight = Hero("Wall-knight", solar, guardian, [])
+nightcap = Hero("Nightcap", kabloom, smarty, [])
+
+
+
 
 hero_db.append(green_shadow)
 
-
-
 xl = pd.read_excel('pvz_dataset.xlsx')
+
+for index, row in xl.iterrows():
+    class_name = row["Class"]
+    for heroclass in hero_class_db:
+        if heroclass.name == class_name:
+            heroclass.cards.append(row.to_dict())
+
+for entry in guardian.cards:
+    print(entry)
+    
+
+
 selected_deck = []
 
-print(xl.head())
+# print(xl.head())
 
 # i = 492
 # while (i <= 1000):
